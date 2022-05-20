831d81ca4d1c4b861f238748f96c91b6fc7626cb
Claudia Vasallo Vega <claudiaw87@gmail.com>
2022-05-20 11:44:07 +0200

    Update README.cmd

--- 1-bfformatting-clinical-data/README.cmd
+++ 1-bfformatting-clinical-data/README.cmd
@@ -1,1 +1,29 @@
 This is to document to conversion of anonymized sample clinical data to Beacon Friendly Format (BFF)
+
+
+Workflow
+
+- Identify variables from the PDF of genetic test (so far)
+- Map variables to Beacon v2 Model: http://docs.genomebeacons.org/
+
+
+  Variables
+  sex
+  age at test
+  date at test
+  variant
+  
+  
+- task needed:
+
+1. extract data
+
+2. format data
+
+2.1 Variant: 
+
+Variant is described as protein HGVS but gene name is in Hugo symbol. We want to format Variant to proper HGVS format and also convert it to standard assembly:chr:pos
+format to be able to search it like that. dbSNP identifiers will also be nice if available.
+
+- convert Gene name to Refseq Id
+- 
