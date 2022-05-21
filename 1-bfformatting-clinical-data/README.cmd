725ed7c7f58f80ee5aaf637de176a90ae57ae188
Claudia Vasallo Vega <claudiaw87@gmail.com>
2022-05-21 15:55:59 +0200

    Update README.cmd

--- 1-bfformatting-clinical-data/README.cmd
+++ 1-bfformatting-clinical-data/README.cmd
@@ -5,9 +5,12 @@ Workflow
 
 - Identify variables from the PDF of genetic test (so far)
 - Map variables to Beacon v2 Model: http://docs.genomebeacons.org/
+- Convert and validate formatted data (BFF)
+- Load BFF jsons to MongoDB
+- Implement Beacon with new MongoDB
 
 
-  Variables
+Target Variables:
 example_data  beacon_variable
 Monsieur  Individual.sex
 gène TTN 	Genomic Variations.molecularAttributes.geneIds
@@ -15,27 +18,34 @@ c.91222_91231del (p.Ser30408Valfs*8)	Genomic Variations.identifiers.proteinHGVSI
 l’état hétérozygote	Genomic Variations.variantLevelData.zigosity
 classe 4	Genomic Variations.caseLevelData.clinicalInterpretations
 syndrome d’Andersen Tawil 	Individual.diseases
-  
-- actions needed:
+
+
+Challenges:
 
 1. extract data
-The data was in non-structured data as free text PDF report. For the proof of concept we did the extraction and translation to bff manually.
-Recommendation: It would be ideal to request that the variables from the genetic test are provided in structured way as follows:
+The data was in non-structured data as free text PDF report. For the proof of concept we did the extraction and translation to bff manually by scraping the target variable values from the PDF.
+
+-recommendation: It would be ideal to request that the variables from the genetic test are provided in structured way as follows:
 gene: Hugo symbol, e.g 
 gHGVS
+
 
 2. format data
 
 2.1 Variant: 
 recommendation: https://varnomen.hgvs.org/
-Variant is described as protein HGVS (https://varnomen.hgvs.org/) but gene name is in Hugo symbol. We want to format Variant to proper HGVS format and also convert it to standard assembly:chr:pos
-format to be able to search it like that. dbSNP identifiers will also be nice if available.
+Variant is described as protein HGVS code (https://varnomen.hgvs.org/). We want to format Variant to proper HGVS format and also convert it to standard VRS
+format. 
 
-- convert Gene name to Refseq Id
-- 
+- actions needed:
+convert HGVS to VRS standard with VRS tool.
+
 
 
 Outcomes:
 2 out of the 3 variants were not in Genomad/ClinVar. recommended: submit to ClinVar: https://www.ncbi.nlm.nih.gov/clinvar/docs/submit/
-Also, a conversion from pHGVS to gHGVS will be needed if data supplier does not provide this data.
+
+Recommendations:
+Require gHGVS and other analysis-related data to genetic test report suppliers, in a structured format, tabular.
+(TODO the full specification of the format)
 
